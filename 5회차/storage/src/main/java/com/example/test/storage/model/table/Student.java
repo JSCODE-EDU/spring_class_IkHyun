9cb3a6405ee16b00005e8a383d8cf0d2fade94c5
DESKTOP-FV1N1HI\user <whdlrgus32@naver.com>
2023-03-26 02:17:55 +0900

    컬럼 설정

--- 5회차/storage/src/main/java/com/example/test/storage/model/table/Student.java
+++ 5회차/storage/src/main/java/com/example/test/storage/model/table/Student.java
@@ -13,7 +13,7 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name")
+    @Column(name = "name", nullable = false)
     private String name;
 
     @Column(name = "class")
